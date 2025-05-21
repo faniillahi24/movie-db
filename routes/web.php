4f939933030cb9dd9a762b49affa0b3e8b409333
fani illahi <faniilahi0@gmail.com>
2025-05-21 16:09:08 +0700

    login

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,25 @@
 <?php
 
-use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+
 Route::get('/', [MovieController::class, 'index']);
 
-Route::get('/movies/{id}', [MovieController::class, 'show'])->name('movies.show');
+Route::get('/movie/{id}/{slug}', [MovieController::class, 'detail_movie']);
 
-Route::get('/movie/create', [MovieController::class, 'create'])->name('movies.create');
-Route::post('/movie', [MovieController::class, 'store'])->name('movies.store');
-Route::get('/', [MovieController::class, 'index'])->name('homepage');
+Route::get('/movie/create', [MovieController::class, 'create'])->middleware('auth');
+
+Route::Post('/movie/store',[MovieController::class,'store'])->middleware('auth');
+
+Route::get('/login', [AuthController::class,'formLogin'])->name('login');
+
+Route::post('/login', [AuthController::class,'login']);
+
+
+Route::get('/movies/{id}', [MovieController::class, 'show'])->name('movies.show');
